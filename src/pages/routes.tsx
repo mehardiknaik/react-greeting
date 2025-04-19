092cc115b16fb58174d472920a9576161477743b
mehardiknaik <hardiknaik7444@gmail.com>
2025-04-19 09:42:53 +0530

    Update routes.tsx

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router';
+import { createHashRouter } from 'react-router';
 import Layout from './Layout';
 import withSuspense from '../HOC/withSuspense';
 
 const Homepage = withSuspense(React.lazy(() => import('./Homepage')));
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Layout />,
